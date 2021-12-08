9ae4a63cb9193835b3bc08d7769a369187b82e51
Samuel Okeke <samwizzy11@gmail.com>
2021-12-08 14:48:29 +0100

    pages responsiveness

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -2,8 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
 
-const materialModules = [MatIconModule, MatTableModule];
+const materialModules = [
+  MatIconModule,
+  MatTableModule,
+  MatButtonModule,
+  MatListModule,
+];
 
 @NgModule({
   declarations: [],
